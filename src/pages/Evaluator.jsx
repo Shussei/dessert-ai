c271c644686dd021c268b8b1dbe576c2158b5ef9
Natal Madekkal <natalmadekkal.2005@gmail.com>
2026-03-13 19:47:48 +0530

    connect frontend to render api

--- src/pages/Evaluator.jsx
+++ src/pages/Evaluator.jsx
@@ -19,7 +19,7 @@ export default function Evaluator() {
     resResult(null)
 
     try {
-      const res = await fetch("http://localhost:3000/evaluate", {
+      const res = await fetch("https://flavormind-api.onrender.com/evaluate", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ recipeText: recipeText })
